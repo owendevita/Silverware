ae4cbecfedf89196e68f2fdd4fdad618c8ac71d7
PickledOwen <owend1117@gmail.com>
2024-02-24 01:17:11 -0800

    setup
    setup

---
+++ Restaurant-Software/restaurant_software/frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './components/Header'
+
+export default function App() {
+  return (
+    <div className ="App">
+      <Header/>
+      MEOW
+    </div>
+  )
+}
---
+++ Restaurant-Software/restaurant_software/frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h1>Our Header</h1>
+  )
+}
+
+export default Header
